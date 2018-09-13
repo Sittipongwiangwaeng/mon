99720b6cec67f78099aef09752c2a136901a133e
Sittipong <mon.15869.2@gmail.com>
2018-09-13 12:33:46 +0700

    cp1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
-import WordCard from './WordCard';
+import CharacterCard from './CharacterCard';
 import './App.css';
 const word = "Hello";
+
 class App extends Component {
- render() {
- 	return (
-    <div>
-        <WordCard value="hello"/>
-    </div>
- 	);
+  
+render() {
+  return (
+  <div className="App">
+  {
+   Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+  }
+   </div>
+  
+  );
+  }
  }
-}
-export default App;
+ export default App;
+ 
